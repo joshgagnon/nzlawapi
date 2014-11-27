554982c572e84c53eba419f38e37424945be35c0
Joshua Gagnon <josh.n.gagnon@gmail.com>
2014-11-27 18:02:09 +1300

    parsing most of the front pages.  need more test cases now

--- parse_case.py
+++ parse_case.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import os
 import json 
 import re
+import pprint
 
 path = '/Users/josh/legislation_archive/justice_trial'
 json_path = '/Users/josh/legislation_archive/justice.json'
@@ -55,14 +56,104 @@ def court(soup):
 	el = next_tag(get_page_one(soup), 'div')
 	reg = re.compile(r'.*OF NEW ZEALAND$')
 	el = [e for e in el.select('div  span') if reg.match(e.text)][0]
-	prev_el = prev_tag(el.parent.parent, 'div').find('span')
-	return '%s %s' % (prev_el.contents[0], el.contents[0])
+	next_el = next_tag(el.parent.parent, 'div').find('span')
+	result = [el.text]
+	if next_el.text != court_file(soup):
+		result +=  [next_el.text]
+	return result
 
-def between(soup):
+def consecutive_align(el):
+	results = []
+	position = re.search(r'.*(left:\d+).*', el.attrs['style']).group(1)
+	while position in el.attrs['style']:
+		results.append(el.text)
+		el = next_tag(el, 'div')
+	return results
+
+def parse_between(soup, el):
+	between_el = [e for e in el.find_all('span') if e.text == 'BETWEEN'][0].parent.parent
+	plantiff = []
+	defendant = []
+	between_el = next_tag(between_el, 'div')
+	while between_el.text != 'AND':
+		plantiff.append(between_el.text)
+		between_el = next_tag(between_el, 'div')
+		
+
+	between_el = next_tag(between_el, 'div')
+	defendant = consecutive_align(between_el)
+
+	return {
+		'plantiff': plantiff,
+		'defendant': defendant
+	}
+			
+
+def parse_versus(soup, el):
+	# must be x v y
+	plantiff = []
+	defendant = []	
+	v = [e for e in el.find_all('span') if e.text == 'v'][0].parent.parent
+	# plantiff is every until neutral citation
+	between_el = prev_tag(v, 'div')
+	cite = neutral_cite(soup)
+	while between_el.text != cite:
+		plantiff = [between_el.text] + plantiff
+		between_el = prev_tag(between_el, 'div')
+	# defendant is everything until no more capitals
+	between_el = next_tag(v, 'div')
+	while between_el.text and between_el.text.upper() == between_el.text:
+		defendant += [between_el.text]
+		between_el = next_tag(between_el, 'div')
+	return {
+		'plantiff': plantiff,
+		'defendant': defendant
+	}
+
+
+def parties(soup):
 	el = next_tag(get_page_one(soup), 'div')
+	if any([e for e in el.find_all('span') if e.text == 'BETWEEN']):
+		return parse_between(soup, el)
+	else:
+		return parse_versus(soup, el)
+
+def judgment_el(soup):
+	judgement_strings = ['Judgment:', 'Sentence:']
+	el = next_tag(get_page_one(soup), 'div')
+	return next_tag([e for e in el.find_all('span') if e.text in judgement_strings][0].parent.parent, 'div')
+
+def judgment(soup):
+	return judgment_el(soup).text
+
+def waistband(soup):
+	return next_tag(judgment_el(soup), 'div').text
+
+def counsel(soup):
+	counsel_strings = ['Counsel:', 'Appearances:']
+	el = next_tag(get_page_one(soup), 'div')
+	counsel = next_tag([e for e in el.find_all('span') if e.text in counsel_strings][0].parent.parent, 'div')
+	return consecutive_align(counsel)
+
+def is_appeal(info):
+	return re.compile('.*NZCA*').match(info['neutral_cite'])
+
+def appeal_result(soup):
+	el = next_tag(next_tag(judgment_el(soup), 'div'), 'div')
+	results = {}
+	position = re.search(r'.*(left:\d+).*', el.attrs['style']).group(1)
+	text_re = re.compile('.*[a-zA-Z]+.*')
+	while position in el.attrs['style']:
+		key = el.text
+		if not text_re.match(key):
+			break
+		el = next_tag(el, 'div')
+		results[key] = el.text
+		el = next_tag(el, 'div')
+	return results	
 
 def process_file(filename):
-	print get_info(filename.replace('.html', ''), json_data)
+	#print get_info(filename.replace('.html', ''), json_data)
 	with open(os.path.join(path, filename)) as f:
 		soup = BeautifulSoup(f.read())
 
@@ -71,9 +162,15 @@ def process_file(filename):
 			'court_file': court_file(soup),
 			'court': court(soup),
 			'full_citation': full_citation(soup),
-			'between': between(soup)
+			'parties': parties(soup),
+			'counsel': counsel(soup),
+			'judgment': judgment(soup),
+			'waistband': waistband(soup)
 		}
-		print results
+		if is_appeal(results):
+			results['appeal_result'] = appeal_result(soup)
+		pprint.pprint(results)
+		print
 
 for f in os.listdir(path):
 	if f.endswith('.html'):
