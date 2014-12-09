ab1308e1ef65a39a6050ab2fb08d649a3e282568
Joshua Gagnon <josh.n.gagnon@gmail.com>
2014-12-09 15:20:11 +1300

    getting hang of react, switching to iuse bootstrap react lib

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,24 @@
 var browser = require('./browser.js');
 var Page = require('./components/page.jsx');
 var Input = require('./components/input.jsx');
+var Select = require('./components/select.jsx');
 var Form = require('./components/form.jsx');
 //var React = require('react');
-/*
+
+
+
+
+var typeValues = [
+	{val: 'act', label: 'Act'},
+	{val: 'case', label: 'Court Case'}
+	]
+
 React.render(
-<Form bsStyle="inline">
-  <Input name="email" type="button" placeholder="Email" required={true}/>
+<Form classes={['hidden-xs', 'legislation_finder']} >
+  <Select name="type" label="Type" options={typeValues} />
+  <Input name="query" type="text" label="Query" />
+  
+  <button id="submit" className="btn btn-primary submit">Search</button>
 </Form>,
-document.body);*/
+document.getElementById('form_wrap'));
+
