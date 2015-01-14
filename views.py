a16dc60364deadf979eaced6e0f8f70b3b382a61
Matthew Cook <mgcook89@gmail.com>
2015-01-15 12:14:16 +1300

    Initial ideas for chart data format and display

--- views.py
+++ views.py
@@ -6,5 +6,6 @@ mod = Blueprint('base', __name__, template_folder='templates')
 @mod.route('/')
 @mod.route('/validator')
 @mod.route('/full_article')
+@mod.route('/graph')
 def browser(act='', query=''):
     return render_template('browser.html')
