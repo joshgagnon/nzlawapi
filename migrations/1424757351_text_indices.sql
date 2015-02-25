f0b3bc78dacb50e634aa5520410381dfe05c9948
Joshua Gagnon <josh.n.gagnon@gmail.com>
2015-02-25 17:55:03 +1300

    pg extension workaround, with an s

--- migrations/1424757351_text_indices.sql
+++ migrations/1424757351_text_indices.sql
@@ -1,4 +1,4 @@
-create extension IF NOT EXIST pg_trgm;
+create extension IF NOT EXISTS pg_trgm;
 create index instrument_name_gin on instruments using gin (title gin_trgm_ops);
 create index case_name_gin on cases using gin (full_citation gin_trgm_ops);
 create index shortcut_name_gin on shortcuts using gin (title gin_trgm_ops);
