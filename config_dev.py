7da89fb2e08c789b8c4c994180dc13198c7bdd4c
Joshua Gagnon <josh.n.gagnon@gmail.com>
2015-03-19 19:35:06 +1300

    reset config

--- config_dev.py
+++ config_dev.py
@@ -25,5 +25,5 @@ USERS_LOGIN_URL = 'https://users.catalex.nz/browser-login'
 SESSION_SECRET = 'secret_here'
 SSO_SHARED_SECRET = 'put_secret_here'
 
-USE_SKELETON = False
+USE_SKELETON = True
 NO_AUTH = True
