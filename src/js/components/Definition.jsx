bfb822259355b118b9ea81298a01165c15d84085
Joshua Gagnon <josh.n.gagnon@gmail.com>
2015-04-01 20:18:32 +1300

    added new def component

---
+++ src/js/components/Definition.jsx
@@ -0,0 +1,37 @@
+"use strict";
+var React = require('react/addons');
+var Glyphicon= require('react-bootstrap/lib/Glyphicon');
+var Reflux = require('reflux');
+var FormStore = require('../stores/FormStore');
+var Actions = require('../actions/Actions');
+var Popover = require('./Popover.jsx');
+var ArticleOverlay= require('./ArticleOverlay.jsx');
+var MQ = require('./Responsive.jsx');
+var NotLatestVersion = require('./Warnings.jsx').NotLatestVersion;
+var ArticleError = require('./Warnings.jsx').ArticleError;
+var Utils = require('../utils');
+var Immutable = require('immutable');
+var DefinitionError = require('./Warnings.jsx').DefinitionError;
+
+
+module.exports = React.createClass({
+    warningsAndErrors: function(){
+        if(this.props.page.getIn(['content', 'error'])){
+            return <DefinitionError error={this.props.page.getIn(['content', 'error'])}/>
+        }
+        return null;
+    },
+    componentDidMount: function(){
+       if(!this.props.page.get('fetching') && !this.props.page.get('fetched')){
+            Actions.requestPage(this.props.page.get('id'));
+       }
+    },
+    render: function(){
+        return <div className="case-container">
+                { this.warningsAndErrors() }
+                {this.props.page.getIn(['content','html']) ?
+                    <div className="case-scaler" dangerouslySetInnerHTML={{__html: this.props.page.getIn(['content','html'])}} /> :
+                    null }
+            </div>
+    }
+ });
