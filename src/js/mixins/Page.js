b2e72fb35a93d03a38fe33c94928a2e6aae6c443
Joshua Gagnon <josh.n.gagnon@gmail.com>
2015-04-06 10:27:57 +1200

    added page mixin

---
+++ src/js/mixins/Page.js
@@ -0,0 +1,23 @@
+var React = require('react/addons');
+var Actions = require('../actions/Actions');
+var $ = require('jquery');
+
+module.exports = {
+    componentDidMount: function(){
+       if(!this.props.page.get('fetching') && !this.props.page.get('fetched')){
+            Actions.requestPage(this.props.page.get('id'));
+       }
+    },
+    componentDidUpdate: function(){
+       if(!this.props.page.get('fetching') && !this.props.page.get('fetched')){
+            Actions.requestPage(this.props.page.get('id'));
+       }
+    },
+    getScrollContainer: function(){
+        return $(React.findDOMNode(this)).parents('.tab-content, .results-container');
+    },
+    overlayOffset: function(){
+        return {'left': this.getScrollContainer().scrollLeft(), 'top': this.getScrollContainer().scrollTop()};
+    },
+
+};
