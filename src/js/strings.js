2d60ccb41a8644d4899bdeefc623b4ed07bb7012
Joshua Gagnon <josh.n.gagnon@gmail.com>
2015-04-14 11:12:18 +1200

    started strings file

---
+++ src/js/strings.js
@@ -0,0 +1,66 @@
+module.exports = {
+    'legislative_instrument': 'Legislative Instrument',
+    'legislative_instruments': 'Legislative Instruments',
+    'act': 'Act',
+    'acts': 'Acts',
+    'bill': 'Bill',
+    'bills': 'Bills',
+
+    'title': 'Title',
+    'contains': 'Contains',
+    'full_citation': 'Full Citation',
+    'all_words': 'All Words',
+    'any_words': 'Any Words',
+    'exact': 'Exact',
+    'bench': 'Bench',
+    'neutral_citation': 'Neutral Citation',
+    'courtfile': 'Court File Number',
+    'court': 'Court',
+    'parties': 'Parties',
+    'matter': 'Matter',
+    'charge': 'Charge',
+    'year': 'Year',
+    'location': 'Location',
+    'public': 'Public',
+    'local': 'Local',
+    'private': 'Private',
+    'provincial': 'Provincial',
+    'imperial': 'Imperial',
+
+    'courts': 'Courts',
+    'supreme_court': 'Supreme Court',
+    'high_court': 'High Court',
+    'appeal_court': 'Appeal Court',
+
+    'all': 'All',
+    'types': 'Type',
+    'status': 'Status',
+
+
+    'act_principal': 'Principal Acts in force',
+    'act_not_in_force': 'Acts not yet in force',
+    'act_amendment_in_force': 'Amendment Acts in force',
+    'act_as_enacted': 'As-enacted Acts',
+    'act_repealed': 'Repealed Acts',
+
+    'current_bills': 'Current Bills',
+    'enacted_bills': 'Enacted Bills',
+    'terminated_bills': 'Terminated Bills',
+
+    'bill_government': 'Government',
+    'bill_local': 'Local',
+    'bill_private': 'Private',
+    'bill_members': 'Member\'s',
+
+    'act_public': 'Public',
+    'act_local': 'Local',
+    'act_private': 'Private',
+    'act_provincial': 'Provincial',
+    'act_imperial': 'Imperial',
+
+    'other_principal': 'Principal Legislative Instruments in force',
+    'other_not_in_force': 'Legislative Instruments not yet in force',
+    'other_amendment_force': 'Amendment Legislative Instruments in force',
+    'other_as_made': 'As-made Legislative Instruments',
+    'other_revoked': 'Revoked Legislative Instruments'
+};
