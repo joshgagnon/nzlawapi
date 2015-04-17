85dd44d8c0d9247c7f8f02636c1a6e2890e9c897
Joshua Gagnon <josh.n.gagnon@gmail.com>
2015-04-17 15:41:37 +1200

    added new result viewers

---
+++ src/js/components/ContainsList.jsx
@@ -0,0 +1,62 @@
+"use strict";
+var React = require('react/addons');
+var Reflux = require('reflux');
+var _ = require('lodash');
+var $ = require('jquery');
+var Actions = require('../actions/Actions');
+var strings = require('../strings');
+var SearchTable = require('../mixins/SearchTable.jsx');
+var PAGE_TYPES = require('../constants').PAGE_TYPES;
+var SEARCH_TYPES = require('../constants').SEARCH_TYPES;
+// New definition result based on this
+var SearchResult = React.createClass({
+    getTitle: function(){
+        return (this.props.data.getIn(['fields','title', 0]) || this.props.data.getIn(['fields','full_citation', 0])) || 'Unknown'
+    },
+    getType: function(){
+        return strings.document_types[this.props.data.getIn(['fields','type', 0])];
+    },
+    getYear: function(){
+        return this.props.data.getIn(['fields','year', 0]);
+    },
+    handleLinkClick: function(e){
+        e.preventDefault();
+        var query = {find: 'contains', doc_type: this.props.data.getIn(['_type']),
+            id: this.props.data.getIn(['fields','id', 0]),
+            contains: this.props.page.getIn(['query', 'contains']),
+            contains_type: this.props.page.getIn(['query', 'contains_type'])
+        };
+        Actions.newPage({
+            query: query,
+            title: this.getTitle() +': '+this.props.page.getIn(['query', 'contains']),
+            page_type: PAGE_TYPES.SEARCH,
+            search_type: PAGE_TYPES.CONTAINS_RESULT,
+        }, this.props.viewer_id);
+    },
+    render: function(){
+        var html = '',
+            id = this.props.data.getIn(['fields', 'id', 0]);
+        /*if( this.props.data.getIn(['highlight'])){
+            html = (this.props.data.getIn(['highlight','document']) ||[]).join('');
+        }*/
+
+        return <tr className="search-result" onClick={this.handleLinkClick}>
+        <td>{ this.props.index + 1}</td>
+        <td><a href={"/open_article/"+this.props.data.get('_type')+'/'+id} >{ this.getTitle() }</a></td>
+        <td> { this.getType() } </td>
+        <td> { this.getYear() }</td>
+        </tr>
+    }
+});
+
+module.exports = React.createClass({
+    mixins: [
+        SearchTable
+    ],
+    renderRow: function(data, index){
+        return <SearchResult index={index} key={data.getIn(['fields', 'id', 0])+''+index} data={data} page={this.props.page} viewer_id={this.props.viewer_id}/>;
+    },
+    render: function(){
+        return this.renderTable();
+    }
+});
---
+++ src/js/components/ContainsResult.jsx
@@ -0,0 +1,48 @@
+"use strict";
+var React = require('react/addons');
+var Input = require('react-bootstrap/lib/Input');
+var Button = require('react-bootstrap/lib/Button');
+var ButtonToolbar = require('react-bootstrap/lib/ButtonToolbar');
+var Actions = require('../actions/Actions');
+var PAGE_TYPES = require('../constants').PAGE_TYPES;
+var ArticleHandlers = require('./ArticleHandlers.jsx');
+var PageMixins = require('../mixins/Page');
+var Popovers = require('./Popovers.jsx');
+
+
+module.exports = React.createClass({
+    mixins: [ArticleHandlers, Popovers, PageMixins],
+    render: function() {
+        var resultContent;
+        if(this.props.page.getIn(['content', 'search_results'])) {
+            if(this.props.page.getIn(['content', 'search_results', 'hits'])) {
+                resultContent = this.props.page.getIn(['content', 'search_results', 'hits']).map(function(r, i) {
+                    return r.getIn(['highlight', 'document']).map(function(html, j){
+                        return <div className="legislation">
+                            <div dangerouslySetInnerHTML={{__html: html}} key={i+'-'+j}/>
+                            </div>
+                    })
+
+                });
+            }
+            else {
+                resultContent = <div className="search-count">No Results Found</div>;
+            }
+        }
+        else if(this.props.page.get('fetching')) {
+            resultContent = <div className="csspinner" />;
+        }
+        else {
+            resultContent = <div className="article-error"><p className="text-danger">{this.props.page.getIn(['content', 'error'])}</p></div>;
+        }
+        var total = this.props.page.getIn(['content', 'search_results', 'total']);
+        return (
+            <div className="search-results legislation-result" onClick={this.interceptLink}>
+                <div className="search-count">{total} Results Found</div>
+                {resultContent}
+                { this.renderFullPopovers({getScrollContainer: this.getScrollContainer}) }
+                { this.renderMobilePopovers() }
+            </div>
+    );
+    }
+});
