f57d2afd99539e731ee0189d3d9ac6d7623e2533
Joshua Gagnon <josh.n.gagnon@gmail.com>
2015-05-24 14:02:12 +1200

    process instruments is now a proper celery task.

--- celeries/tasks.py
+++ celeries/tasks.py
@@ -3,18 +3,32 @@ from psycopg2 import extras
 from celeries.celery import app
 from db import get_db
 import server
+from acts import links
+from acts import queries
 
 
 @app.task
-def process_instrument(document_id):
+def process_instrument(document_ids):
     with server.app.test_request_context():
         db = get_db()
-        with db.cursor(cursor_factory=extras.RealDictCursor) as cur:
-            query = """SELECT *, exists(select 1 from latest_instruments where id=i.id) as latest FROM instruments i
-                        JOIN documents d on d.id = i.id
-                        where processed_document is null
-                        and i.id = %(id)s """
+        title_store = links.get_links(db)
 
-            cur.execute(query, {'id': document_id})
-            result = cur.fetchall()
-            return dict(result)
+        def get_links(tree, db):
+            return links.process_instrument_links(tree, db, links=title_store)
+
+        with db.cursor(cursor_factory=extras.RealDictCursor) as cur:
+            for document_id in document_ids:
+                query = """SELECT *, exists(select 1 from latest_instruments where id=i.id) as latest FROM instruments i
+                            JOIN documents d on d.id = i.id
+                            where processed_document is null
+                            and i.id = %(id)s """
+
+                cur.execute(query, {'id': document_id})
+                result = cur.fetchall()
+                if len(result):
+                    queries.process_instrument(
+                        row=result[0], db=db,
+                        refresh=False,
+                        latest=result[0].get('latest'),
+                        strategy={'links': get_links})
+    return 'done'
--- scripts/process_instruments.py
+++ scripts/process_instruments.py
@@ -7,101 +7,54 @@ import os
 from multiprocessing import Pool
 from lxml import etree
 import logging
-
-
-def get_db(config_filename):
-    config = importlib.import_module(config_filename.replace('.py', ''), 'parent')
-    db = psycopg2.connect(
-        database=config.DB,
-        user=config.DB_USER,
-        host=config.DB_HOST,
-        password=config.DB_PW)
-    db.set_client_encoding('utf8')
-    return db
-
-
-def get_total(db):
-    with db.cursor(cursor_factory=extras.RealDictCursor) as cur:
-        query = """select count(*) as count from instruments i join documents d on i.id = d.id where processed_document is null """
-        cur.execute(query)
-        return cur.fetchone()['count']
-
+from time import sleep
 
 def chunks(l, n):
     for i in xrange(0, len(l), n):
         yield l[i:i+n]
 
 
-def run_process((config_filename, ids)):
-    from acts import acts
-    from acts import queries
-    from acts import links
-    import server
-    sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
-    print 'doing %d jobs' % len(ids)
-
-    db = get_db(config_filename)
-
-    link_store = links.get_links(db)
-
-    def get_links(tree, db):
-        return links.process_instrument_links(tree, db, links=link_store)
-
-    with db.cursor(cursor_factory=extras.RealDictCursor) as cur, server.app.test_request_context():
-        for document_id in ids:
-            #print 'fetching %d for processing' % document_id
-            query = """SELECT *, exists(select 1 from latest_instruments where id=i.id) as latest FROM instruments i
-                    JOIN documents d on d.id = i.id
-                    where processed_document is null
-                    and i.id = %(id)s """
-
-            cur.execute(query, {'id': document_id})
-            result = cur.fetchall()
-            if not len(result):
-                continue
-
-            queries.process_instrument(
-                row=result[0], db=db,
-                refresh=False,
-                latest=result[0].get('latest'),
-                strategy={'links': get_links})
-    print 'finished %d docs' % len(ids)
-
-    db.rollback()
-    db.close()
-
-
 if __name__ == "__main__":
     if not len(sys.argv) > 1:
         raise Exception('Missing configuration file')
     sys.path.append(os.getcwd())
-    db = get_db(sys.argv[1])
-    logging.basicConfig(level=logging.INFO)
+    config = importlib.import_module(sys.argv[1].replace('.py', ''), 'parent')
 
+    sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+    from celeries.tasks import process_instrument
+
+    logging.basicConfig(level=logging.INFO)
+    db = psycopg2.connect(
+            database=config.DB,
+            user=config.DB_USER,
+            host=config.DB_HOST,
+            password=config.DB_PW)
+    tasks = []
     try:
         with db.cursor(cursor_factory=extras.RealDictCursor) as cur:
             query = """SELECT i.id as id FROM instruments i
                     JOIN documents d on d.id = i.id
-                    where processed_document is null """
+                    where processed_document is null"""
             cur.execute(query)
             results = cur.fetchall()
         if len(results):
             print '%s documents to process' % len(results)
-            processes = min(int(sys.argv[2] if len(sys.argv) > 2 else 2), len(results))
-            pool = Pool(processes=processes)
-            args = map(lambda x: (sys.argv[1], x), (list(chunks(map(lambda x: x['id'], results),
-                       len(results) / processes + len(results) % processes))))
-
-            pool.map_async(run_process, (args))
-            pool.close()
+            jobs = list(chunks([r['id'] for r in results], 100))
+            tasks = [process_instrument.delay(j) for j in jobs]
+            while True:
+                tasks_finished = len(filter(lambda x: x.ready(), tasks))
+                sys.stdout.write('%d%%\r' % (tasks_finished/float(len(jobs))*100))
+                sys.stdout.flush()
+                if tasks_finished == len(jobs):
+                    break
+                sleep(2)
         else:
             print 'Nothing to do'
 
     except KeyboardInterrupt:
-        pool.terminate()
         print "Keyboard interrupt in main"
-    else:
-        pool.join()
+        for task in tasks:
+            task.revoke()
     finally:
         print "Cleaning up Main"
         with db.cursor(cursor_factory=extras.RealDictCursor) as cur:
