314596c3a6d8b3a3abfdc3246b1b102de5babd2f
Joshua Gagnon <josh.n.gagnon@gmail.com>
2015-08-08 15:39:16 +1200

    another tweak to commit

--- celeries/tasks.py
+++ celeries/tasks.py
@@ -41,9 +41,9 @@ def process_instrument(document_ids):
 @app.task
 def process_skeleton(document_ids):
     with server.app.test_request_context():
-        for document_id in document_ids:
-            db = get_db()
-            with db.cursor(cursor_factory=extras.RealDictCursor) as cur:
+        db = get_db()
+        with db.cursor(cursor_factory=extras.RealDictCursor) as cur:
+            for document_id in document_ids:
                 print 'fetching %d for skeletizing' % document_id
                 query = """SELECT *, exists(select 1 from latest_instruments where id=i.id) as latest FROM instruments i
                         JOIN documents d on d.id = i.id
@@ -56,5 +56,5 @@ def process_skeleton(document_ids):
                     tree = etree.fromstring(result[0]['processed_document'], parser=large_parser)
                     queries.process_skeleton(result[0].get('id'), tree, version=result[0].get('version'), db=db)
 
-            db.commit()
-            db.close()
+                db.commit()
+    db.close()
